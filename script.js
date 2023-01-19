f74bb03e679e16fdf8a5f642a1acacf0fd996710
Deshan Diwelgama <deshanvikum@gmail.com>
2023-01-19 20:27:03 +1100

    added playGame functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let computerScore = 0,
+  playerScore = 0;
 
 function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -9,41 +11,47 @@ function playRound(playerSelection, computerSelection) {
   //Trim and change the player selection to lowercase
   playerSelection = playerSelection.trim().toLowerCase();
   computerSelection = computerSelection.trim().toLowerCase();
-  let output;
-  //If player selected rock
-  if (playerSelection == "rock") {
-    output =
-      computerSelection == "rock"
-        ? "That's a draw"
-        : computerSelection == "paper"
-        ? "Paper beats rock, you lose!"
-        : "Rock beats scissors, You win you rock!";
-
-    return output;
-  } //If the player selects paper
-  else if (playerSelection == "paper") {
-    output =
-      computerSelection == "rock"
-        ? "You win. Paper beats rock"
-        : computerSelection == "paper"
-        ? "It's a draw"
-        : "Scissors cut paper. you lose";
-
-    return output;
-  } //If the player selects scissors
-  else if (playerSelection == "scissors") {
-    output =
-      computerSelection == "rock"
-        ? "Rock beats scissors. You lose"
-        : computerSelection == "paper"
-        ? "Scissors beat paper. you win!"
-        : "It's a draw!";
-
-    return output;
+  let outputMessage;
+  //If both select the same thing
+  if (playerSelection == computerSelection) {
+    outputMessage = "It's a draw";
+    console.log(outputMessage);
+    return;
+  } //The  player winning scenarios
+  else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    playerScore++;
+    outputMessage = `${playerSelection} beats ${computerSelection}. Good Job!`;
+    console.log(outputMessage);
+    return;
+  } //The computer winning scenarios
+  else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+    computerScore++;
+    outputMessage = `${computerSelection} beats ${playerSelection}. You Lose!`;
+    console.log(outputMessage);
+    return;
   } else {
-    return "No valid input";
+    outputMessage = "No valid input";
+    console.log(outputMessage);
+    return;
   }
 }
 
-const playerSelection = "rock";
-console.log(playRound(playerSelection, getComputerChoice()));
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let playerInput = prompt("Type in 'rock', 'paper' or 'scissors'");
+    playRound(playerInput, getComputerChoice());
+  }
+  computerScore > playerScore
+    ? console.log("Better Luck Next time")
+    : console.log("Congrats, you won");
+}
+
+playGame();
